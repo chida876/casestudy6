7f5ddcb68d526d4be2d0376b3c89a68f21c38a3c
chida876 <116781445+chida876@users.noreply.github.com>
2024-05-07 09:56:21 +0530

    Create shellscript.sh

---
+++ shellscript.sh
@@ -0,0 +1,14 @@
+# In the user_data section of the EC2 instance resource
+user_data = <<-EOF
+#!/bin/bash
+# Install AWS CLI
+yum install -y aws-cli
+
+# Shell script to read files from S3 bucket
+aws s3 cp s3://your_bucket_name/ /path/to/local/directory --recursive
+
+# Install CloudWatch Logs agent
+wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py
+chmod +x ./awslogs-agent-setup.py
+./awslogs-agent-setup.py -n -r your_aws_region -c /path/to/cloudwatch-config-file.cfg
+EOF
