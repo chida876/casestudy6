d14b69681aa2efa9f634ca8433c6e31df241f90d
chida876 <116781445+chida876@users.noreply.github.com>
2024-05-07 09:55:09 +0530

    Update vpc_endpoint.tf

--- vpc_endpoint.tf
+++ vpc_endpoint.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_endpoint" "s3_endpoint" {
-  vpc_id            = aws_vpc.my_vpc.id
+  vpc_id            = aws_vpc.case6_vpc.id
   service_name      = "com.amazonaws.your_region.s3"
   route_table_ids   = [aws_route_table.my_route_table.id]
   policy            = <<POLICY
